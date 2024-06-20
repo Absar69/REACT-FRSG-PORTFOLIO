9c5f8597394db3cdc30f6179ab6e448e3e13d6ec
habiba <f219099@cfd.nu.edu.pk>
2024-06-20 23:37:13 +0500

    react app initialised

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// App.js
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            {/* Your component JSX here */}
+        </div>
+    );
+}
+
+export default App;
