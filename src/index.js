42327d093e216343921a5cff7ae99b249613b037
habiba <f219099@cfd.nu.edu.pk>
2024-07-09 16:17:14 +0500

    Remove all files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import App from './App';
-import './index.css'
-// index.js or App entry point
-import { createRoot } from 'react-dom';
-
-
-createRoot(document.querySelector('#root')).render(<App />);
