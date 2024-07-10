4de3997caa0c1b0fa85c0cdcb63e5e3ccde488a4
habiba <f219099@cfd.nu.edu.pk>
2024-07-10 21:08:36 +0500

    Contact component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import { Contact } from "./components/Contact";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Contact />
     </div>
   );
 }
